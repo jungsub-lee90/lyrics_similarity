e3aa28a16d17f4b42d8a80b5bb75586d47a2e8fb
jungsub.lee90 <jungsub.lee90@gmail.com>
2023-03-02 02:41:40 +0900

    web lyrics

---
+++ flask_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##export FLASK_APP=sesac
+##export FLASK_DEBUG=true
+##export FLASK_RUN_PORT=8999
+##export FLASK_RUN_HOST=0.0.0.0
+
+##source ~/.bashrc 배치 파일 변경 사항 후 저장
+##source activate python3
+##flask run
+source lyrics/bin/activate
+python3 app.py
